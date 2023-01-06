3748a3e11e3991400b77436b259ea124414ca475
kien8995 <trandongkien.sine@gmail.com>
2023-01-06 22:46:57 +0700

    update

--- src/modules/maersk/rates.py
+++ src/modules/maersk/rates.py
@@ -1,12 +1,16 @@
+from selenium.webdriver import DesiredCapabilities
 from seleniumwire import webdriver
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--ignore-certificate-errors-spki-list')
 chrome_options.add_argument('--ignore-ssl-errors')
 # chrome_options.add_argument('--proxy-server=45.119.80.22:8087')
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--remote-debugging-port=9222')
+# chrome_options.add_argument("--proxy-server={}".format('host.docker.internal:8098'))
 
 chrome_capabilities = {
     "browserName": "chrome",
@@ -15,7 +19,6 @@ chrome_capabilities = {
     "javascriptEnabled": True,
     'applicationName': 'test1'
 }
-
 
 fireFoxOptions = webdriver.FirefoxOptions()
 fireFoxOptions.headless = True
@@ -28,6 +31,21 @@ seleniumwire_options = {
     }
 }
 
+# def start_browser():
+#     # Ensure mobile-friendly view for parsing
+#     useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+# 
+#     profile = webdriver.FirefoxProfile()
+#     profile.set_preference("general.useragent.override", useragent)
+#     options = webdriver.FirefoxOptions()
+#     options.set_preference("dom.webnotifications.serviceworker.enabled", False)
+#     options.set_preference("dom.webnotifications.enabled", False)
+#     options.add_argument('--headless')
+# 
+#     browser = webdriver.Firefox(firefox_profile=profile, options=options)
+#     return browser
+
+
 ##  Get the URL
 # driver = webdriver.Chrome(executable_path="/workspaces/hpg-ibe/ungoogled-chromium_108.0.5359.98_1.vaapi_linux/chrome", chrome_options=chrome_options)
 # seleniumwire_options = {
@@ -36,23 +54,53 @@ seleniumwire_options = {
 #         'addr': '45.119.80.22:4444',
 #         'port': 8087
 #     }
+
+# useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
+# profile = webdriver.FirefoxProfile()
+# profile.set_preference("general.useragent.override", useragent)
+
+PROXY = "45.119.80.22:55466"
+webdriver.DesiredCapabilities.CHROME['proxy'] = {
+    "httpProxy": PROXY,
+    "ftpProxy": PROXY,
+    "sslProxy": PROXY,
+    "proxyType": "MANUAL",
+
+}
+
+webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
+
+# driver = webdriver.Remote(
+#     command_executor="http://45.119.80.22:4444",
+#     options=chrome_options,
+#     # desired_capabilities=chrome_capabilities,
+#     # seleniumwire_options=seleniumwire_options
+#     desired_capabilities={
+#         "browserName": "chrome",
+#         "javascriptEnabled": True,
+#         "acceptInsecureCerts": True,
+#         "moz:debuggerAddress": True
+#     },
+# 
+
+firefox_options = webdriver.FirefoxOptions()
+firefox_options.log.level = "trace"
+firefox_options.binary_location = "/usr/bin/firefox"
+firefox_options.add_argument("--headless")
+# 
 driver = webdriver.Remote(
-    command_executor="http://45.119.80.22:4444",
-    options=fireFoxOptions,
-    # desired_capabilities=chrome_capabilities,
-    # seleniumwire_options=seleniumwire_options
-    desired_capabilities={
-            "browserName": "firefox",
-            "browserVersion": "latest",
-            "video": True,
-            "platform": "ANY",
-            "javascriptEnabled": True
-        }
-    )
-driver.get("https://www.maersk.com/instantPrice")
+    desired_capabilities=DesiredCapabilities.CHROME,
+    command_executor="http://45.119.80.22:9515",
+    options=chrome_options
+)
+
+
+driver.get("https://www.w3coded.com")
 
 ##  Print request headers
 for request in driver.requests:
     print(request.url)  # <--------------- Request url
     print(request.headers)  # <----------- Request headers
     print(request.response.headers)  # <-- Response headers
+
+driver.quit()
